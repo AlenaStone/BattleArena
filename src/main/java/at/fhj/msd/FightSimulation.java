c2b42420860e0e9d4475c0e22a638fa6b16ea2a9
Alena-Vodopianova <alena.vodopianova@edu.fh-joanneum.at>
2025-05-22 20:25:40 +0200

    Fixed errors in the method createFighters

--- src/main/java/at/fhj/msd/FightSimulation.java
+++ src/main/java/at/fhj/msd/FightSimulation.java
@@ -13,10 +13,10 @@ public class FightSimulation {
     public void createFighters() {
         System.out.println("Hi. It's game \"Battle Area\". ");
         System.out.println("+---------+-------+------------");
-        System.out.print("Choose how much player do you need (1,2 or 3): ");
-        
+        System.out.print("Choose how much player do you need (2,3 or 4): ");
+
         String NumberOfPlayer = scanner.nextLine();
-        if(!NumberOfPlayer.equals("1") && !NumberOfPlayer.equals("2") && !NumberOfPlayer.equals("3")) {
+        if (!NumberOfPlayer.equals("4") && !NumberOfPlayer.equals("2") && !NumberOfPlayer.equals("3")) {
             System.out.println("Please enter a valid number of players (1, 2, or 3).");
             return;
         }
@@ -26,22 +26,41 @@ public class FightSimulation {
             System.out.print("Choose the name of player " + (i + 1) + ": ");
             String name = scanner.nextLine();
             System.out.println("+---------+-------+------------");
-            System.out.print("Choose the type of player (Archer, Mage, Warrior): ");
+
+            System.out.println("1.Mage");
+            System.out.println("2.Warrior");
+            System.out.println("3.Archer");
+            System.out.print("Choose the type of player: ");
             String type = scanner.nextLine();
 
-            if (type.equalsIgnoreCase("Archer")) {
+            if (type.equalsIgnoreCase("Archer") || type.equals("3")) {
                 fighter = new Archer(name);
-            } else if (type.equalsIgnoreCase("Mage")) {
+
+            } else if (type.equalsIgnoreCase("Mage") || type.equals("1")) {
                 fighter = new Mage(name);
 
-            } else if (type.equalsIgnoreCase("Warrior")) {
+            } else if (type.equalsIgnoreCase("Warrior") || type.equals("2")) {
                 fighter = new Warrior(name);
+
             } else {
                 System.out.println("Unknown fighter type, please try again.");
                 i--;
                 continue;
             }
             fighters.add(fighter);
+            System.out.println(name + " choose " + fighter.getType());
+              System.out.println("+---------+-------+------------");
+            System.out.println("Characteristics of your " + fighter.getType() + " :");
+            System.out.println("Your power: " + fighter.getPower());
+            System.out.println("Your speed: " + fighter.getSpeed());
+            System.out.println("Your HP: " + fighter.getHp());
+             System.out.println("+---------+-------+------------");
+
+
         }
+    }
+
+    public void startFight() {
+
     }
 }
